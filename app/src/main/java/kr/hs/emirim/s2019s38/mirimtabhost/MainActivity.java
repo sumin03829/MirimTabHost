ac282b3d82be3b56a92a3e96e9ab92ba3cd9a794
sumin_030829 <sumin0829@kako.com>
2020-11-09 19:33:33 +0900

    Complete TabHost

--- app/src/main/java/kr/hs/emirim/s2019s38/mirimtabhost/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimtabhost/MainActivity.java
@@ -2,13 +2,31 @@ package kr.hs.emirim.s2019s38.mirimtabhost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost=getTabHost();
+
+        TabHost.TabSpec tabSpecPark= tabHost.newTabSpec("Park").setIndicator("박세리");
+        tabSpecPark.setContent(R.id.linear_park);
+        tabHost.addTab(tabSpecPark);
+
+        TabHost.TabSpec tabSpecSon= tabHost.newTabSpec("Park").setIndicator("손예진");
+        tabSpecSon.setContent(R.id.linear_park);
+        tabHost.addTab(tabSpecSon);
+
+        TabHost.TabSpec tabSpecAhn= tabHost.newTabSpec("Park").setIndicator("안현무");
+        tabSpecAhn.setContent(R.id.linear_park);
+        tabHost.addTab(tabSpecAhn);
+
+        tabHost.setCurrentTab(0);
     }
 }
