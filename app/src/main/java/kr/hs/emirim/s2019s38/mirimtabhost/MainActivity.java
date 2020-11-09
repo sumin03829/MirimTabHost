a7ee79cb11465ee0054e481e234e28d4f5d492bb
sumin_030829 <sumin0829@kako.com>
2020-11-09 19:23:26 +0900

    create TabHost Layout

---
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimtabhost/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019s38.mirimtabhost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
