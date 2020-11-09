bbd7947ed4d921a5800b290fac66e758a449598e
sumin_030829 <sumin0829@kako.com>
2020-11-09 20:26:21 +0900

    Init WebViewActivity

---
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimtabhost/WabViewActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019s38.mirimtabhost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WabViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wab_view);
+    }
+}
